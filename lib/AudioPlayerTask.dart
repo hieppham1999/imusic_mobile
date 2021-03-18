ca4495c429c00382fa035c05fd92cd146e22c747
hieppt99 <58847533+hieppham1999@users.noreply.github.com>
2021-03-19 00:04:37 +0700

    first run success

--- lib/AudioPlayerTask.dart
+++ lib/AudioPlayerTask.dart
@@ -6,11 +6,18 @@ import 'package:just_audio/just_audio.dart';
 import 'package:imusic_mobile/MediaLibrary.dart';
 import 'package:imusic_mobile/Seeker.dart';
 
+
+// Must be a top-level function
+void backgroundTaskEntrypoint() {
+  AudioServiceBackground.run(() => AudioPlayerTask());
+}
+
 class AudioPlayerTask extends BackgroundAudioTask{
   //
 
   final _mediaLibrary = MediaLibrary();
   final _player = AudioPlayer();
+  final _completer = Completer();
   AudioProcessingState _skipState;
   Seeker _seeker;
   StreamSubscription<PlaybackEvent> _eventSubscription;
@@ -19,169 +26,56 @@ class AudioPlayerTask extends BackgroundAudioTask{
   int get index => _player.currentIndex;
   MediaItem get mediaItem => index == null ? null : queue[index];
 
-//   @override
-//   Future<void> onStart(Map<String, dynamic> params) async {
-//     // We configure the audio session for speech since we're playing a podcast.
-//     // You can also put this in your app's initialisation if your app doesn't
-//     // switch between two types of audio as this example does.
-//     final session = await AudioSession.instance;
-//     await session.configure(AudioSessionConfiguration.speech());
-//     // Broadcast media item changes.
-//     _player.currentIndexStream.listen((index) {
-//       if (index != null) AudioServiceBackground.setMediaItem(queue[index]);
-//     });
-//     // Propagate all events from the audio player to AudioService clients.
-//     _eventSubscription = _player.playbackEventStream.listen((event) {
-//       _broadcastState();
-//     });
-//     // Special processing for state transitions.
-//     _player.processingStateStream.listen((state) {
-//       switch (state) {
-//         case ProcessingState.completed:
-//         // In this example, the service stops when reaching the end.
-//           onStop();
-//           break;
-//         case ProcessingState.ready:
-//         // If we just came from skipping between tracks, clear the skip
-//         // state now that we're ready to play.
-//           _skipState = null;
-//           break;
-//         default:
-//           break;
-//       }
-//     });
-//
-//     // Load and broadcast the queue
-//     AudioServiceBackground.setQueue(queue);
-//     try {
-//       await _player.setAudioSource(ConcatenatingAudioSource(
-//         children:
-//         queue.map((item) => AudioSource.uri(Uri.parse(item.id))).toList(),
-//       ));
-//       // In this example, we automatically start playing on start.
-//       onPlay();
-//     } catch (e) {
-//       print("Error: $e");
-//       onStop();
-//     }
-//   }
-//
-//   @override
-//   Future<void> onSkipToQueueItem(String mediaId) async {
-//     // Then default implementations of onSkipToNext and onSkipToPrevious will
-//     // delegate to this method.
-//     final newIndex = queue.indexWhere((item) => item.id == mediaId);
-//     if (newIndex == -1) return;
-//     // During a skip, the player may enter the buffering state. We could just
-//     // propagate that state directly to AudioService clients but AudioService
-//     // has some more specific states we could use for skipping to next and
-//     // previous. This variable holds the preferred state to send instead of
-//     // buffering during a skip, and it is cleared as soon as the player exits
-//     // buffering (see the listener in onStart).
-//     _skipState = newIndex > index
-//         ? AudioProcessingState.skippingToNext
-//         : AudioProcessingState.skippingToPrevious;
-//     // This jumps to the beginning of the queue item at newIndex.
-//     _player.seek(Duration.zero, index: newIndex);
-//     // Demonstrate custom events.
-//     AudioServiceBackground.sendCustomEvent('skip to $newIndex');
-//   }
-//
-//   @override
-//   Future<void> onPlay() => _player.play();
-//
-//   @override
-//   Future<void> onPause() => _player.pause();
-//
-//   @override
-//   Future<void> onSeekTo(Duration position) => _player.seek(position);
-//
-//   @override
-//   Future<void> onFastForward() => _seekRelative(fastForwardInterval);
-//
-//   @override
-//   Future<void> onRewind() => _seekRelative(-rewindInterval);
-//
-//   @override
-//   Future<void> onSeekForward(bool begin) async => _seekContinuously(begin, 1);
-//
-//   @override
-//   Future<void> onSeekBackward(bool begin) async => _seekContinuously(begin, -1);
-//
-//   @override
-//   Future<void> onStop() async {
-//     await _player.dispose();
-//     _eventSubscription.cancel();
-//     // It is important to wait for this state to be broadcast before we shut
-//     // down the task. If we don't, the background task will be destroyed before
-//     // the message gets sent to the UI.
-//     await _broadcastState();
-//     // Shut down this task
-//     await super.onStop();
-//   }
-//
-//   /// Jumps away from the current position by [offset].
-//   Future<void> _seekRelative(Duration offset) async {
-//     var newPosition = _player.position + offset;
-//     // Make sure we don't jump out of bounds.
-//     if (newPosition < Duration.zero) newPosition = Duration.zero;
-//     if (newPosition > mediaItem.duration) newPosition = mediaItem.duration;
-//     // Perform the jump via a seek.
-//     await _player.seek(newPosition);
-//   }
-//
-//   /// Begins or stops a continuous seek in [direction]. After it begins it will
-//   /// continue seeking forward or backward by 10 seconds within the audio, at
-//   /// intervals of 1 second in app time.
-//   void _seekContinuously(bool begin, int direction) {
-//     _seeker?.stop();
-//     if (begin) {
-//       _seeker = Seeker(_player, Duration(seconds: 10 * direction),
-//           Duration(seconds: 1), mediaItem)
-//         ..start();
-//     }
-//   }
-//
-//   /// Broadcasts the current state to all clients.
-//   Future<void> _broadcastState() async {
-//     await AudioServiceBackground.setState(
-//       controls: [
-//         MediaControl.skipToPrevious,
-//         if (_player.playing) MediaControl.pause else MediaControl.play,
-//         MediaControl.stop,
-//         MediaControl.skipToNext,
-//       ],
-//       systemActions: [
-//         MediaAction.seekTo,
-//         MediaAction.seekForward,
-//         MediaAction.seekBackward,
-//       ],
-//       androidCompactActions: [0, 1, 3],
-//       processingState: _getProcessingState(),
-//       playing: _player.playing,
-//       position: _player.position,
-//       bufferedPosition: _player.bufferedPosition,
-//       speed: _player.speed,
-//     );
-//   }
-//
-//   /// Maps just_audio's processing state into into audio_service's playing
-//   /// state. If we are in the middle of a skip, we use [_skipState] instead.
-//   AudioProcessingState _getProcessingState() {
-//     if (_skipState != null) return _skipState;
-//     switch (_player.processingState) {
-//       case ProcessingState.idle:
-//         return AudioProcessingState.stopped;
-//       case ProcessingState.loading:
-//         return AudioProcessingState.connecting;
-//       case ProcessingState.buffering:
-//         return AudioProcessingState.buffering;
-//       case ProcessingState.ready:
-//         return AudioProcessingState.ready;
-//       case ProcessingState.completed:
-//         return AudioProcessingState.completed;
-//       default:
-//         throw Exception("Invalid state: ${_player.processingState}");
-//     }
-//   }
-// }
+  @override
+  Future<void> onStart(Map<String, dynamic> params) async {
+    AudioServiceBackground.setState(
+        controls: [MediaControl.pause, MediaControl.stop],
+        playing: true,
+        processingState: AudioProcessingState.connecting);
+    // Connect to the URL
+    await _player.setUrl("https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3");
+    // Now we're ready to play
+    _player.play();
+    // Broadcast that we're playing, and what controls are available.
+    AudioServiceBackground.setState(
+        controls: [MediaControl.pause, MediaControl.stop],
+        playing: true,
+        processingState: AudioProcessingState.ready);
+  }
+
+  @override
+  Future<void> onStop() async {
+    // Stop playing audio
+    await _player.stop();
+    // Broadcast that we've stopped.
+    await AudioServiceBackground.setState(
+        controls: [],
+        playing: false,
+        processingState: AudioProcessingState.stopped);
+    // Shut down this background task
+    await super.onStop();
+  }
+
+  @override
+  Future<void> onPlay() async {
+    // Broadcast that we're playing, and what controls are available.
+    AudioServiceBackground.setState(
+        controls: [MediaControl.pause, MediaControl.stop],
+        playing: true,
+        processingState: AudioProcessingState.ready);
+    // Start playing audio.
+    await _player.play();
+  }
+
+  @override
+  Future<void> onPause() async {
+    // Broadcast that we're paused, and what controls are available.
+    AudioServiceBackground.setState(
+        controls: [MediaControl.play, MediaControl.stop],
+        playing: false,
+        processingState: AudioProcessingState.ready);
+    // Pause the audio.
+    await _player.pause();
+  }
+
+ }
--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,6 +1,8 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 
+import 'AudioPlayerTask.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -15,17 +17,41 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Container(
         padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: start, child: Text("Start")),
-            ElevatedButton(onPressed: stop, child: Text("Stop")),
-          ],
+        child: StreamBuilder<PlaybackState>(
+            stream: AudioService.playbackStateStream,
+            builder: (context, snapshot) {
+              final playing = snapshot.data?.playing ?? false;
+              final processingState = snapshot.data?.processingState
+                  ?? AudioProcessingState.stopped;
+              return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  if (playing)
+                    ElevatedButton(child: Text("Pause"), onPressed: pause)
+                  else
+                    ElevatedButton(child: Text("Play"), onPressed: play),
+                  if (processingState != AudioProcessingState.stopped)
+                    ElevatedButton(child: Text("Stop"), onPressed: stop),
+                  ],
+              );
+            },
         ),
       ),
     );
   }
   start() =>
-      AudioService.start(backgroundTaskEntrypoint: _backgroundTaskEntrypoint);
+      AudioService.start(backgroundTaskEntrypoint: backgroundTaskEntrypoint);
   stop() => AudioService.stop();
+
+  play() async {
+    if (await AudioService.running) {
+      AudioService.play();
+    } else {
+      AudioService.start(backgroundTaskEntrypoint: backgroundTaskEntrypoint);
+    }
+  }
+
+  pause() => AudioService.pause();
+
+
 }
