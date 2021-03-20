6e5c1cafed330683dca7dc825762d439f05563b7
hieppt99 <58847533+hieppham1999@users.noreply.github.com>
2021-03-20 17:41:04 +0700

    add seeker, next queue item

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,7 +1,11 @@
 import 'package:audio_service/audio_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:rxdart/rxdart.dart';
+import 'SeekBar.dart';
 import 'AudioPlayerTask.dart';
+import 'MediaState.dart';
+import 'package:imusic_mobile/QueueState.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -15,30 +19,95 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('iMusic'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: StreamBuilder<PlaybackState>(
-            stream: AudioService.playbackStateStream,
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: StreamBuilder<PlaybackState>(
+                stream: AudioService.playbackStateStream,
+                builder: (context, snapshot) {
+                  final playing = snapshot.data?.playing ?? false;
+                  final processingState = snapshot.data?.processingState
+                      ?? AudioProcessingState.stopped;
+                  return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      if (playing)
+                        pauseButton()
+                      else
+                        playButton(),
+                      if (processingState != AudioProcessingState.stopped)
+                        stopButton(),
+                      ],
+                  );
+                },
+            ),
+          ),
+          // Seek Bar
+          StreamBuilder<MediaState>(
+            stream: _mediaStateStream,
             builder: (context, snapshot) {
-              final playing = snapshot.data?.playing ?? false;
-              final processingState = snapshot.data?.processingState
-                  ?? AudioProcessingState.stopped;
-              return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  if (playing)
-                    ElevatedButton(child: Text("Pause"), onPressed: pause)
-                  else
-                    ElevatedButton(child: Text("Play"), onPressed: play),
-                  if (processingState != AudioProcessingState.stopped)
-                    ElevatedButton(child: Text("Stop"), onPressed: stop),
-                  ],
+              final mediaState = snapshot.data;
+              return SeekBar(
+                duration:
+                  mediaState?.mediaItem?.duration ?? Duration.zero,
+                position: mediaState?.position ?? Duration.zero,
+                onChangeEnd: (newPosition) {
+                  AudioService.seekTo(newPosition);
+                },
               );
             },
-        ),
+          ),
+          // Display the processing state.
+          StreamBuilder<AudioProcessingState>(
+            stream: AudioService.playbackStateStream
+                .map((state) => state.processingState)
+                .distinct(),
+            builder: (context, snapshot) {
+              final processingState =
+                  snapshot.data ?? AudioProcessingState.none;
+              return Text(
+                  "Processing state: ${describeEnum(processingState)}");
+            },
+          ),
+          // Display the latest custom event.
+          StreamBuilder(
+            stream: AudioService.customEventStream,
+            builder: (context, snapshot) {
+              return Text("custom event: ${snapshot.data}");
+            },
+          ),
+          // Display the notification click status.
+          StreamBuilder<bool>(
+            stream: AudioService.notificationClickEventStream,
+            builder: (context, snapshot) {
+              return Text(
+                'Notification Click Status: ${snapshot.data}',
+              );
+            },
+          ),
+        ],
       ),
     );
   }
+
+  /// A stream reporting the combined state of the current media item and its
+  /// current position.
+  Stream<MediaState> get _mediaStateStream =>
+      Rx.combineLatest2<MediaItem, Duration, MediaState>(
+          AudioService.currentMediaItemStream,
+          AudioService.positionStream,
+              (mediaItem, position) => MediaState(mediaItem, position));
+
+  /// A stream reporting the combined state of the current queue and the current
+  /// media item within that queue.
+  Stream<QueueState> get _queueStateStream =>
+      Rx.combineLatest2<List<MediaItem>, MediaItem, QueueState>(
+          AudioService.queueStream,
+          AudioService.currentMediaItemStream,
+              (queue, mediaItem) => QueueState(queue, mediaItem));
+
+
   start() =>
       AudioService.start(backgroundTaskEntrypoint: backgroundTaskEntrypoint);
   stop() => AudioService.stop();
@@ -53,5 +122,21 @@ class _HomeScreenState extends State<HomeScreen> {
 
   pause() => AudioService.pause();
 
+  IconButton playButton() => IconButton(
+    icon: Icon(Icons.play_arrow),
+    iconSize: 64.0,
+    onPressed: play,
+  );
 
+  IconButton pauseButton() => IconButton(
+    icon: Icon(Icons.pause),
+    iconSize: 64.0,
+    onPressed: pause,
+  );
+
+  IconButton stopButton() => IconButton(
+    icon: Icon(Icons.stop),
+    iconSize: 64.0,
+    onPressed: stop,
+  );
 }
