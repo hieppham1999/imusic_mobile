c5f2f1506f564d2f92babe9eb64657cce2d13b6f
hieppt99 <58847533+hieppham1999@users.noreply.github.com>
2021-04-22 00:41:52 +0700

    fetch data success!

--- lib/MediaLibrary.dart
+++ lib/MediaLibrary.dart
@@ -1,34 +1,28 @@
+import 'dart:convert';
+
 import 'package:audio_service/audio_service.dart';
+import 'package:dio/dio.dart' as Dio;
+import 'package:imusic_mobile/models/myMediaItem.dart';
+import 'package:imusic_mobile/services/dio.dart';
 
 class MediaLibrary {
-  final _items = <MediaItem>[
-    // MediaItem(
-    //   id: "https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3",
-    //   album: "Science Friday",
-    //   title: "A Salute To Head-Scratching Science",
-    //   artist: "Science Friday and WNYC Studios",
-    //   duration: Duration(milliseconds: 5739820),
-    //   artUri: Uri.parse("https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
-    // ),
-    // MediaItem(
-    //   id: "https://s3.amazonaws.com/scifri-segments/scifri201711241.mp3",
-    //   album: "Science Friday",
-    //   title: "From Cat Rheology To Operatic Incompetence",
-    //   artist: "Science Friday and WNYC Studios",
-    //   duration: Duration(milliseconds: 2856950),
-    //   artUri: Uri.parse("https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
-    // ),
-    // MediaItem(
-    //   id: "https://1285a08e157e.ngrok.io/storage/media/songs/24H---LyLy;-Magazine.mp3",
-    //   album: "24Ha",
-    //   title: "24H",
-    //   artist: "Lyly; The Magazine",
-    //   duration: Duration(milliseconds: 257000),
-    //   artUri: null,
-    // ),
-  ];
 
-  List<MediaItem> get items => _items;
+
+  Future<List<MyMediaItem>> fetchItem(String url) async {
+
+      try {
+        Dio.Response response = await dio().get(url);
+        return (response.data as List)
+            .map((x) => MyMediaItem.fromJson(x))
+            .toList();
+      } catch (e, stacktrace) {
+        print('caught $e : $stacktrace');
+      }
+      return [];
+  }
+
+
+
 
 
 }
--- lib/pages/home_tab.dart
+++ lib/pages/home_tab.dart
@@ -1,41 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:audio_service/audio_service.dart';
+import 'package:imusic_mobile/MediaLibrary.dart';
 import 'package:imusic_mobile/components/song_listview.dart';
+import 'package:imusic_mobile/models/myMediaItem.dart';
 import '../AudioPlayerTask.dart';
 import '../music_player.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
+
+  @override
+  _HomeTabState createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  MediaLibrary _newSong = new MediaLibrary();
+  List<MyMediaItem>? items = [];
+  var loading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchSongs();
+  }
+
+  void fetchSongs() async {
+    setState(() {
+      loading = true;
+    });
+    var updatedList = (await (_newSong.fetchItem('/songs')));
+    setState(() {
+      items = updatedList;
+      loading = false;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    final items = <MediaItem>[
-      MediaItem(
-        id: "https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3",
-        album: "Science Friday",
-        title: "A Salute To Head-Scratching Science",
-        artist: "Science Friday and WNYC Studios",
-        duration: Duration(milliseconds: 5739820),
-        artUri: Uri.parse(
-            "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
-      ),
-      MediaItem(
-        id: "http://192.168.0.150:8000/storage/media/songs/24H---LyLy;-Magazine.mp3",
-        album: "24H (Single)",
-        title: "24H",
-        artist: "Lyly; The Magazine",
-        duration: Duration(milliseconds: 257000),
-        artUri: Uri.parse(
-            "http://192.168.0.150:8000/storage/media/album_arts/24H---LyLy;-Magazine.jpg"),
-      ),
-      MediaItem(
-        id: "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3",
-        album: "Ninja Tuna",
-        title: "Kalimba",
-        artist: "Mr. Scruff",
-        duration: Duration(milliseconds: 348000),
-        artUri: Uri.parse(
-            "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
-      ),
-    ];
+
+
+    // final items = <MediaItem>[
+    //   MediaItem(
+    //     id: "https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3",
+    //     album: "Science Friday",
+    //     title: "A Salute To Head-Scratching Science",
+    //     artist: "Science Friday and WNYC Studios",
+    //     duration: Duration(milliseconds: 5739820),
+    //     artUri: Uri.parse(
+    //         "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
+    //   ),
+    //   MediaItem(
+    //     id: "http://192.168.0.150:8000/storage/media/songs/24H---LyLy;-Magazine.mp3",
+    //     album: "24H (Single)",
+    //     title: "24H",
+    //     artist: "Lyly; The Magazine",
+    //     duration: Duration(milliseconds: 257000),
+    //     artUri: Uri.parse(
+    //         "http://192.168.0.150:8000/storage/media/album_arts/24H---LyLy;-Magazine.jpg"),
+    //   ),
+    //   MediaItem(
+    //     id: "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3",
+    //     album: "Ninja Tuna",
+    //     title: "Kalimba",
+    //     artist: "Mr. Scruff",
+    //     duration: Duration(milliseconds: 348000),
+    //     artUri: Uri.parse(
+    //         "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
+    //   ),
+    // ];
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -52,11 +85,11 @@ class HomeTab extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
-                itemCount: items.length,
+                itemCount: items!.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) => songListView(
                       context,
-                      items[index],
+                      items![index],
                     )),
           ),
 
@@ -64,5 +97,4 @@ class HomeTab extends StatelessWidget {
       ),
     );
   }
-
 }
